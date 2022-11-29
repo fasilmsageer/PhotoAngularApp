67a2d6b66239a17e31ccccb7976a9af8fb44014c
fasilmsageer <fasilmsageer@gmail.com>
2022-11-29 15:09:31 +0530

    view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPhotoComponent } from './view-photo/view-photo.component';
+
+const myRoute=[
+  {
+    path:"",
+    component:ViewPhotoComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPhotoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
